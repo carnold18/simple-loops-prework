1fa83d7908c67d2fc9502019323a214f7c6fd53a
Christopher Arnold <chrisarnold2010@gmail.com>
2018-06-25 14:34:06 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -26,10 +26,10 @@ def while_iterator(number_of_times)
 end
 
 def until_iterator(number_of_times)
-  # code your solution here using the "until" keyword
-  # hint: use a counter to tell the until loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  number_of_times = 0
+  until number_of_times = 7 do
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
 end
 
 def for_iterator(number_of_times)
