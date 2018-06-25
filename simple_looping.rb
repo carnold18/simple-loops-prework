24e3bee25ba4cf3a32ecce0b3123d05f93b35ac6
Christopher Arnold <chrisarnold2010@gmail.com>
2018-06-25 14:44:45 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -34,7 +34,7 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
-  for number_of_times in (0..6)
+  for number_of_times in (1..7)
     puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
